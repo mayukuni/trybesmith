c7d9697123f0fe770c2a685527232e3ae3967ba8
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2022-09-13 18:16:32 -0300

    feat: adiciona endpoint para listagem de produtos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import ProductsRoutes from './routes/products.route';
 
 const app = express();
 
 app.use(express.json());
+app.use(ProductsRoutes);
 
 export default app;
